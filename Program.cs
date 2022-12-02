b75a35ea2319a2a57508b18c8a6ed27fc8ce2ee8
Nikita <nikita.serg.utkin@gmail.com>
2022-12-02 23:44:56 +0900

    Работает

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int m = 0;
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        m = num1;
+    }
+
+}
+else if (num2 > num3)
+{
+    m = num2;
+}
+else
+{
+    m = num3;
+}
+Console.WriteLine($"Максимальное число {m}");
+
